aa52b8b17408c88ff32e4e1f4558daa54ed5c88d
alejandro-vazquez <avi200599@gmail.com>
2024-04-26 14:39:10 -0600

    [IMP] API: insert restaurants data from csv
    Was created a script to insert data from a csv file to the database

---
+++ db/scripts/insert_restaurants.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+file = 'db/scripts/restaurantes.csv'
+
+CSV.foreach(file, headers: true) do |restaurant|
+  restaurant = Restaurant.create(
+    rating: restaurant['rating'].to_i,
+    name: restaurant['name'],
+    site: restaurant['site'],
+    email: restaurant['email'],
+    phone: restaurant['phone'],
+    street: restaurant['street'],
+    city: restaurant['city'],
+    state: restaurant['state'],
+    lat: restaurant['lat'].to_f,
+    lng: restaurant['lng'].to_f
+  )
+  puts restaurant.name
+end
